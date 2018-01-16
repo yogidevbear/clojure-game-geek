8441d752d8752d20fb0a2475a4ffb9691e1bb1cc
Andrew Jackson <Andrew Jackson>
2018-01-16 22:31:29 +0000

    Initial commit. Updated default project.clj and README.md files. Added initial cgg-schema.edn file

---
+++ README.md
@@ -0,0 +1,14 @@
+# clojure-game-geek
+
+My personal repository for working through the lacinia docs tutorial http://lacinia.readthedocs.io/en/latest/tutorial/index.html
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2018
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clojure-game-geek "0.1.0-SNAPSHOT"
+  :description "Working through the lacinia docs tutorial http://lacinia.readthedocs.io/en/latest/tutorial/index.html"
+  :url "https://github.com/yogidevbear/clojure-game-geek"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.walmartlabs/lacinia "0.21.0"]])
